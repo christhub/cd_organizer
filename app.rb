2f216c99780d5ab5d5e0352eb8565b9ff20fafca
andrew dang & christian lei <student@epicodus.com>
2015-08-12 14:49:42 -0700

    finish building site

--- app.rb
+++ app.rb
@@ -1,21 +1,30 @@
 require('sinatra')
-require('sinatra-contrib')
+require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/cd')
 
 get('/') do
-  @cd = CD.all()
+  erb(:index)
+end
+
+get('/cds') do
+  @cds = CD.all()
   erb(:cds)
 end
 
-get('/cds/new') do
+get ('/cds/new') do
   erb(:cd_form)
 end
 
-post('/cds') do
+post ('/cds') do
   title = params.fetch("title")
-  year = params.fetch("year")
+  year = params.fetch("year").to_s()
   cd = CD.new(title, year)
   cd.save()
   erb(:success)
+end
+
+get('/cds/:id') do
+  @cd = CD.find(params.fetch('id').to_i())
+  erb(:cd)
 end
