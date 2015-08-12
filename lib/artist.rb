4c7430526e2e39f16d85b083ee74f5c0c1a01d46
andrew dang & christian lei <student@epicodus.com>
2015-08-12 15:10:36 -0700

    add all method and pass spec

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,18 @@
 class Artist
+  @@artists = []
+
   define_method(:initialize) do |name|
     @name = name
   end
 
   define_method(:name) do
     @name
+  end
+
+  # define_method(:id) do
+
+  define_singleton_method(:all) do
+   @@artists
   end
 
 end
--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -10,4 +10,22 @@ describe(Artist) do
     end
   end
 
+  # describe('#id') do
+  #   it('returns the id of the artist') do
+  #     test_artist = Artist.new('Tame Impala')
+  #     expect(test_artist.id()).to(eq(1))
+  #   end
+  # end
+  #
+  # describe('#cd') do
+  #   it('returns an empty array of cds for the artist') do
+  #     test_artist
+  #   end
+
+  describe('.all') do
+    it('returns an empty array') do
+     expect(Artist.all()).to(eq([]))
+   end
+ end
+
 end
